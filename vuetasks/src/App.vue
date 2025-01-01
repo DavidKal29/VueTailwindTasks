7f8d63076e441a6c7bb7e381f29e700982f20bc2
David Kalmuk <correo@correo.com>
2025-01-01 11:23:55 +0100

    Iniciamos proyecto Vue con Tailwindcss

---
+++ vuetasks/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <MainComponent></MainComponent>
+</template>
+
+<script>
+import MainComponent from './components/MainComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    MainComponent
+  }
+}
+</script>
+
---
+++ vuetasks/src/components/MainComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="bg-red-500">Hola a todos</section>
+</template>
+
+<script>
+export default {
+    name:'MainComponent'
+}
+</script>
+
+<style>
+
+</style>
